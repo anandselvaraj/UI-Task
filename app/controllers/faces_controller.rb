ea10db3dec3b56e6c6003d3307d4e66c403eb57e
sudhagara2 <sudhagara2@gmail.com>
2016-08-20 12:18:49 +0530

    first commit

---
+++ app/controllers/faces_controller.rb
@@ -0,0 +1,229 @@
+require 'csv'
+class FacesController < ApplicationController
+  def index
+      @face=Face.all
+  end
+   def audit_report
+      @face=Face.new
+    end
+    def audit_process 
+      wu=[];f1=[];f2=[];f3=[];f4=[];f5=[];f6=[];f7=[];f8=[];f9=[];f10=[];f11=[];f12=[];file1=[];fmonth=[],anual=[];reporttyp=[]
+    anual=params[:faces][:year]
+    reporttyp=params[:faces][:reporttype]
+    fmonth=params[:faces][:month]
+    f1 << params[:faces][:aa]
+    f2 << params[:faces][:bb]
+    f3 << params[:faces][:cc]
+    f4 << params[:faces][:dd]
+    f5 << params[:faces][:ee]
+    f6 << params[:faces][:ff]
+    f7 << params[:faces][:gg]
+    f8 << params[:faces][:hh]
+    f9 << params[:faces][:ii]
+    f10 << params[:faces][:jj]
+    f11 << params[:faces][:kk]
+    f12 << params[:faces][:ll]
+    f1 << params[:faces][:a]
+    f2 << params[:faces][:b]
+    f3 << params[:faces][:c]
+    f4 << params[:faces][:d]
+    f5 << params[:faces][:e]
+    f6 << params[:faces][:f]
+    f7 << params[:faces][:g]
+    f8 << params[:faces][:h]
+    f9 << params[:faces][:i]
+    f10 << params[:faces][:j]
+    f11 << params[:faces][:k]
+    f12 << params[:faces][:l]
+    f1 << params[:faces][:aaa]
+    f2 << params[:faces][:bbb]
+    f3 << params[:faces][:ccc]
+    f4 << params[:faces][:ddd]
+    f5 << params[:faces][:eee]
+    f6 << params[:faces][:fff]
+    f7 << params[:faces][:ggg]
+    f8 << params[:faces][:hhh]
+    f9 << params[:faces][:iii]
+    f10 << params[:faces][:jjj]
+    f11 << params[:faces][:kkk]
+    f12 << params[:faces][:lll]
+    file1 = f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12
+    @face=Face.audit_task(file1,fmonth,anual,reporttyp)
+    if reporttyp=="Monthly"
+    redirect_to "/files/#{fmonth}_#{anual}_audit_report.csv"
+    else 
+    redirect_to "/files/#{anual}_audit_report.csv"
+    end
+   end
+  def duplication
+    @dup=Face.new
+  end
+  def duplication_process
+    file1=params[:face][:file_1]
+    filename=params[:face][:file_1].original_filename
+    if params[:face][:file_1].original_filename =~ /.\.csv$/i
+    @dup=Face.dups(file1)
+    redirect_to "/files/Repetitive_output.csv"
+    else
+    redirect_to faces_vert_help_path
+    end
+
+  end
+  def vertical
+   @face=Face.new
+  end
+
+  def vert_process
+    file1=params[:face][:file_1]
+    filename=params[:face][:file_1].original_filename
+    if params[:face][:file_1].original_filename =~ /.\.csv$/i
+    @face=Face.verticals(file1)
+    redirect_to "/files/Vertical_Output.csv"
+    else 
+    redirect_to faces_vert_help_path
+    end
+  end
+  def compare
+      @face=Face.new
+  end
+  def save_file
+   file1=params[:face][:file_1]
+   file2=params[:face][:file_2]
+   filename=params[:face][:file_1].original_filename
+   filename=params[:face][:file_2].original_filename
+    if params[:face][:file_1].original_filename =~ /.\.csv$/i and params[:face][:file_2].original_filename =~ /.\.csv$/i
+      @face=Face.com_vertical(file1,file2)
+      @face=Face.comparison
+      redirect_to "/files/Output_comparison.csv"
+    else
+      redirect_to faces_vert_help_path
+    end
+  end
+
+=begin  def spellcheck
+    @face=Face.new
+  end
+ 
+ def spell_process
+   if params[:face][:file_1].original_filename =~ /.\.csv$/i and params[:face][:file_2].original_filename =~ /.\.csv$/i
+      file1=params[:face][:file_1]
+      filename=params[:face][:file_1].original_filename
+      @face=Face.spell_check(file1,)
+      redirect_to :action=>"/process/Speller.rb"
+   else
+      redirect_to faces_vert_help_path
+   end
+  end
+=end
+  def uom
+      @face=Face.new
+  end
+
+  def uom_method
+    file1=params[:face][:file_1]
+    file2=params[:face][:file_2]
+    filename=params[:face][:file_1].original_filename
+    filename=params[:face][:file_2].original_filename
+    if params[:face][:file_1].original_filename =~ /.\.csv$/i and params[:face][:file_2].original_filename =~ /.\.csv$/i
+       @face=Face.uoms(file1,file2)
+       redirect_to "/files/UOM_file_Output.csv"
+    else 
+      redirect_to faces_vert_help_path
+    end
+  end
+
+  def attribute_extraction
+    @face=Face.new
+  end
+
+  def attribute_extraction_process
+    file1=params[:face][:file_1]
+    file2=params[:face][:file_2]
+    filename=params[:face][:file_1].original_filename
+    filename=params[:face][:file_2].original_filename
+    if params[:face][:file_1].original_filename =~ /.\.csv$/i and params[:face][:file_2].original_filename =~ /.\.csv$/i
+       @face=Face.extract_attributes(file1,file2)
+       redirect_to "/files/Attribute_extracted_Output.csv"
+    else
+      redirect_to faces_vert_help_path
+    end
+  end
+
+ def pilot
+file1=params[:face][:file3]
+      file2=params[:face][:file4]
+
+      filename=params[:face][:file3].original_filename
+      filename=params[:face][:file4].original_filename
+     
+      @face=Face.pil2(file1,file2)
+    
+      unless @face.blank?
+
+      flash[:notice] = "Your file has been successfully imported."
+
+      redirect_to "/files/output.csv"
+   
+      else
+
+       flash[:notice] = "Invalid file headers or file format"
+     
+       redirect_to :action=> "welookup"
+    
+      end
+end
+
+def consicutive
+  @face=Face.new
+end
+
+  def consicutive_process
+ if params[:face][:file_1].original_filename =~ /.\.csv$/i 
+      file1=params[:face][:file_1]
+      filename=params[:face][:file_1].original_filename
+      @face=Face.consecutive(file1,filename)
+      redirect_to "/files/Output_#{filename}"
+   else
+      redirect_to faces_vert_help_path
+   end
+  end
+
+
+
+  def attribute_extraction
+    @face=Face.new
+  end
+
+  def attribute_extraction_process
+    file1=params[:face][:file_1]
+    file2=params[:face][:file_2]
+    filename=params[:face][:file_1].original_filename
+    filename=params[:face][:file_2].original_filename
+    if params[:face][:file_1].original_filename =~ /.\.csv$/i and params[:face][:file_2].original_filename =~ /.\.csv$/i
+       @face=Face.extract_attributes(file1,file2)
+       redirect_to "/files/Output_file.csv"
+    else
+      redirect_to faces_vert_help_path
+    end
+  end
+
+
+
+
+  def vert_help
+      @face=Face.all
+  end
+
+  def result
+      @face=Face.new
+  end
+
+  def help_ver
+      @face=Face.new
+  end
+
+  def help_uom
+      @face=Face.new
+  end
+
+end
---
+++ app/models/face.rb
@@ -0,0 +1,488 @@
+require 'roo'
+require 'spreadsheet'
+require 'iconv'
+require 'csv'
+class Face < ActiveRecord::Base
+  def self.com_vertical(file1,file2)
+  t=0
+   CSV.open("#{Rails.root}/public/files/Vertical_Audit.csv","w") do |csv|
+   csv << ["Item ID","Name","Value","Unit"]
+   CSV.foreach(file1.tempfile,:encoding=>("ISO8859"))do |line|
+        t=t+1
+       next if t == 1
+        x = line ; id=x.shift
+        ad=[]
+        ad=x.each_slice(3).to_a
+        ad.each do|p|
+       break if p[0] == nil
+       unless p[1] == nil
+        csv << [id] + p
+       end
+       end
+       end
+    end
+
+   t=0
+    CSV.open("#{Rails.root}/public/files/Vertical_Shipment.csv","w") do |csv|
+    csv << ["Item ID","Name","Value","Unit"]
+    CSV.foreach(file2.tempfile,:encoding=>("ISO8859"))do |line|
+        t=t+1
+       next if t == 1
+        x = line ; id=x.shift
+        ad=[]
+        ad=x.each_slice(3).to_a
+        ad.each do|p|
+       break if p[0] == nil
+       unless p[1] == nil
+        csv << [id] + p
+       end
+       end
+       end
+   end
+ end
+ def self.audit_task(file1,fmonth,anual,reporttyp)
+  if reporttyp=="Monthly"
+  tns=[]
+  name=[]
+  n=1;t=0;
+  a=[];b=[];c=[];d=[];e=[];g=[];quality=[];final=[];a3=[];first=[];
+  name1=[];a1=[];a2=[];totsq=[];totsa=[];totsater=[];err=[];qual=[];totsqu=[];totsam=[];qualper=[];errper=[];errp=[];qualp=[];toterr=[];toter=[];final=[]
+  totsqur=[];totsamr=[];toterrr=[],totpr=[];totp=[];check=[]
+  CSV.open("#{Rails.root}/public/out/outpro.csv" ,"w") do |csv|
+  csv << ["Name","Projectnames","Total sku","Sample count","FinalErrorCount","Error(%)","Quality(%)"]
+    file1.each do|kfile|
+     unless kfile[0] == nil
+        ff=CSV.read(kfile[0].tempfile)
+          check=ff[0][0]        
+        if check=="Name"
+        ff.each_with_index do |row ,i| 
+          next if i==0
+          csv << [row[0],row[1],row[2].to_f,row[3].to_f,row[4]]
+        end
+      else
+        wui=kfile[n].to_f
+        projectname=kfile[2]   
+        ff=CSV.read(kfile[0].tempfile)
+        ff.shift
+        name=ff.collect{|row| row[1]}.uniq
+
+           name.each do |i|
+            @tns=ff.select{|row| i==row[1]}
+            ff.each do|row|
+              if i==row[1] && row[2] != nil
+                c << row [2]
+                next if row[3] == nil || row[3] == "NOTA" || row[8] == "Updated/Not Uptated" || row[8] == "Not Updated" 
+                e << row[8]
+              end
+                 b=@tns.length
+                 g=e.length
+                 d=c.length
+                 a=g.to_f/d.to_f*100.0
+                 quality=100.0-a.to_f
+                if wui ==7 || wui ==""
+                  b=@tns.length
+                  g=e.length
+                  d=c.length
+                  a=(g.to_f/d.to_f*100.0).round(2)
+                  quality=(100.0-a.to_f).round(2)
+                else
+                  b=(wui/7*b).round(2)
+                  d=(wui/7*d).round(2)
+                  g=g
+                  a=(g.to_f/d.to_f*100.0).round(2)
+                  quality=(100.0-a.to_f).round(2)
+                end
+            end
+              c.clear
+              e.clear
+              csv << [i,projectname,b,d,g,a,quality]
+          end
+      end    
+    end
+  end
+end
+
+    CSV.open("#{Rails.root}/public/files/#{fmonth}_#{anual}_audit_report.csv" ,"w") do |final|
+    final << ["Name","ProjectNames","Total sku","Sample count","FinalErrorCount","Error(%)","Quality(%)"]    
+    z=CSV.read("#{Rails.root}/public/out/outpro.csv")
+      a1=z.collect{|s| s[0]}.uniq
+       a1.each_with_index do |p,index|
+          next if index == 0
+            z.each do |s|
+              if p==s[0]
+                totp << s[1]
+                totsq << s[2].to_f
+                totsa << s[3].to_f
+                toter << s[4].to_f
+                err << s[5].to_f
+                qual << s[6].to_f
+              end
+                totsqu = totsq.inject(:+)
+                totsam = totsa.inject(:+)
+                toterr = toter.inject(:+)
+                totsqur = (totsqu.to_f).round(2)
+                totsamr = (totsam.to_f).round(2)
+                toterrr = (toterr.to_f).round(2)
+                qualp = qual.inject(:+)
+                errp = err.inject(:+)
+                errper = (toterr.to_f/totsam.to_f*100.0).round(2)
+                qualper = (100-errper).round(2)
+                totpr=totp.uniq.to_s.gsub!(/[^0-9A-Za-z,-,]/, '')
+            end
+              totsq.clear
+              totsa.clear
+              toter.clear
+              err.clear
+              qual.clear
+              totp.clear
+              final << [p,totpr,totsqur,totsamr,toterrr,errper,qualper]
+        end
+        
+   end
+else
+  tns=[]
+  name=[]
+  n=1;t=0;
+  a=[];b=[];c=[];d=[];e=[];g=[];quality=[];final=[];a3=[];first=[];
+  name1=[];a1=[];a2=[];totsq=[];totsa=[];totsater=[];err=[];qual=[];totsqu=[];totsam=[];qualper=[];errper=[];errp=[];qualp=[];toterr=[];toter=[];final=[]
+  totsqur=[];totsamr=[];toterrr=[],totpr=[];totp=[]
+  CSV.open("#{Rails.root}/public/out/outpro.csv" ,"w") do |csv|
+  csv << ["Name","Projectnames","Total sku","Sample count","FinalErrorCount","Error(%)","Quality(%)"]
+    file1.each do|kfile|
+     unless kfile[0] == nil
+        ff=CSV.read(kfile[0].tempfile)
+        ff.each_with_index do |row ,i| 
+          next if i==0
+          csv << [row[0],row[1],row[2].to_f,row[3].to_f,row[4]]
+        end    
+    end
+  end
+end
+    CSV.open("#{Rails.root}/public/files/#{anual}_audit_report.csv" ,"w") do |final|
+    final << ["Name","ProjectNames","Total sku","Sample count","FinalErrorCount","Error(%)","Quality(%)"]    
+    z=CSV.read("#{Rails.root}/public/out/outpro.csv")
+      a1=z.collect{|s| s[0]}.uniq
+
+       a1.each_with_index do |p,index|
+          next if index == 0
+            z.each do |s|
+              if p==s[0]
+                totp << s[1]
+                totsq << s[2].to_f
+                totsa << s[3].to_f
+                toter << s[4].to_f
+                err << s[5].to_f
+                qual << s[6].to_f
+              end
+                totsqu = totsq.inject(:+)
+                totsam = totsa.inject(:+)
+                toterr = toter.inject(:+)
+                totsqur = (totsqu.to_f).round(2)
+                totsamr = (totsam.to_f).round(2)
+                toterrr = (toterr.to_f).round(2)
+                qualp = qual.inject(:+)
+                errp = err.inject(:+)
+                errper = (toterr.to_f/totsam.to_f*100.0).round(2)
+                qualper = (100-errper).round(2)
+                totpr=totp.uniq.to_s.gsub(/\W/,'')
+              end
+              totsq.clear
+              totsa.clear
+              toter.clear
+              err.clear
+              qual.clear
+              totp.clear
+              final << [p,totpr,totsqur,totsamr,toterrr,errper,qualper]
+        end
+   end
+end
+end    
+ def self.comparison
+  File.open("#{Rails.root}/public/files/Output_comparison.csv","w") do |c| 
+  a=File.open("#{Rails.root}/public/files/Vertical_Audit.csv","r",:headers=>'true').collect{|o|o.chomp}
+  b=File.open("#{Rails.root}/public/files/Vertical_Shipment.csv","r",:headers=>'true').collect{|o|o.chomp}
+    if a != b
+      c.puts b - a
+    end
+    end
+ end
+     
+ def self.uoms(file1,file2)
+  t=0
+   CSV.open("#{Rails.root}/public/files/Vertical_UOM_Audit.csv","w") do |csv|
+   csv << ["Item ID","Name","Value","Unit"]
+   CSV.foreach(file1.tempfile,:encoding=>("ISO8859"))do |line|
+
+       t=t+1
+    next if t == 1
+     x = line ; id=x.shift
+     ad=[]
+     ad=x.each_slice(3).to_a
+     ad.each do|p|
+    break if p[0] == nil
+    unless p[1] == nil
+     csv << [id] + p
+    end
+     end
+     end
+     end
+
+      s=0
+      t=0
+      CSV.open("#{Rails.root}/public/files/UOM_file_Output.csv","w") do |csv|
+      csv << ["Item ID","Name","Value","Unit"]
+        a=CSV.read("#{Rails.root}/public/files/Vertical_UOM_Audit.csv")
+        b=CSV.read(file2.tempfile)
+         
+          a.each do |i|
+          t=t+1
+            next if t==1
+            if i[3]!=nil
+                 b.each do |j|
+                 s=j
+            break if i[3]==s[0]
+            end
+            unless i[3]==s[0]
+                 csv << i
+            end
+          end
+        end
+      end
+  end
+   
+   def self.verticals(file1)
+     
+   CSV.open("#{Rails.root}/public/files/Vertical_Output.csv","w+") do |csv|
+   csv << ["Item ID","Name","Value","Unit"]
+   CSV.foreach(file1.tempfile,:encoding => 'iso-8859-1:utf-8')do |line|
+   next if line[0].to_s.match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil
+      
+      x = line ; id=x.shift
+
+      sd = x.shift(5) 
+
+      sdd = sd.each_slice(1).to_a
+
+      dd = ["HTML","PT Sourced PDF","RS Website","RSPDF","SKU Accounting Flag"] 
+
+      ad = x.each_slice(3).to_a 
+
+      add = dd,sd 
+
+      ad1 = add.reduce(&:zip).map(&:flatten) 
+
+      ad3 = ad1 + ad 
+
+      ad3.each do|p|
+
+    break if p[0] == nil
+    unless p[1] == nil
+      csv << [id] + p 
+    end
+    end
+    end
+    end
+   end
+
+=begin   def self.spell_check(file1)
+    #  speller = FFI::Hunspell.dict('en_GB')
+       f=File.open(file1.tempfile,"r").read
+           f=f.split(" ").uniq!
+       f.each do |i|
+          next if i == i.upcase
+          next if  i =~ /\d/
+           i = i.strip
+           i = i.gsub(/\W+/,' ')
+           if speller.check?(i)
+
+           else
+              File.open("/home/altius/anand-ror/Firstop.csv","a+"){|file|file.puts("#{i}")}
+           end
+     end
+            f=File.open("/home/altius/anand-ror/Firstop.csv","r").read
+              f=f.split(" ").uniq!
+               f.each do |i|
+                 next if i == i.upcase
+                   next if  i =~ /\d/
+                     next if  i.length <= 2
+                       i = i.strip
+                         i = i.gsub(/\W+/,' ')
+                 if speller.check?(i)
+
+                  else
+                    puts "#{i.chomp}"
+               File.open("/home/altius/mahesh/UI_Task/public/files/Output_Spellcheck.csv","w+") do |c|
+               c.puts("#{i}")
+                 end
+              end
+
+
+   end
+=end
+
+def self.consecutive(file1,filename)
+out=[]
+f=File.open(file1.tempfile,"r").read
+ File.open("#{Rails.root}/public/files/Output_#{filename}","w+")do|ss| #{|file|file.puts("#{f[k]+" "+f[k+1]}")}
+f=f.split(" ")
+#f  = f.gsub(/\W+/,'')
+#next if  i =~ /\d/
+i=(f.length)-1
+i.times do |k|
+     # f[k]= f[k].gsub(/\W+/,'')
+      # f[k+1]= f[k+1].gsub(/\W+/,'')
+     if f[k].downcase.gsub(/\W+/,'')==f[k+1].downcase.gsub(/\W+/,'')
+    print " "
+    print f[k]+" "+f[k+1]#,f[k+2]
+   #  else
+   #  puts false
+#  file= File.open("/home/altius/mahesh/UI_Task/public/files/Consecutive_output.csv","a+")
+   ss.puts  "#{f[k]+" "+f[k+1]+" "}"
+ # file << "#{f[k]+" "+f[k+1]}"
+    end
+      end
+   end
+  end
+def self.dups(file1)
+ CSV.open("#{Rails.root}/public/files/Repetitive_output.csv", "w") do |csv|
+   ff= CSV.read(file1.tempfile)
+     @cc=ff.count
+     ff.last(@cc-2).each do|i|
+      if "#{i[2]}".include?("|")
+       @c3="#{i[2]}".split("|")
+
+        @c4= @c3.map{|i| i.strip}
+
+        @c1=@c4.uniq.join("\n")
+
+
+         @det =  [i[0]] + [i[1]] + [@c1]
+       else
+           @c="#{i[2]}".split("\n").uniq
+
+           @i=@c.shift
+            @ss=@c.map{|i| i.split(@i)}.uniq
+
+           @c1=([@i]+[@ss]).join(" \n")
+         @det =  [i[0]] + [i[1]] + [@c1]
+       end
+
+         csv << @det
+
+     end
+  end
+
+end
+=begin def self.reading_xl(file1,file2)
+      
+      b = Spreadsheet.open(file1.tempfile,"r")
+   
+    sheet=b.worksheet(0)
+   
+    file=File.open("/home/altius/mahesh/UI_Task/public/files/Audit_convert.csv","w")
+      
+    sheet.each do |i|
+    if i != nil
+      i="#{i}".gsub("nil", '')
+      i= "#{i}".gsub!(/[^0-9A-Za-z,-,]/, '')
+      file.puts "#{i}"
+     
+      end
+    
+     end
+     b = Spreadsheet.open(file2.tempfile,"r")
+   
+    sheet=b.worksheet(0)
+   
+    file=File.open("/home/altius/mahesh/UI_Task/public/files/Ship_convert.csv","w")
+      
+    sheet.each do |i|
+    if i != nil
+      i="#{i}".gsub("nil", '')
+      i= "#{i}".gsub!(/[^0-9A-Za-z,-,]/, '')
+      file.puts "#{i}"
+     
+      end
+    
+     end
+=end  end
+
+def self.pil2(file1,file2)
+  #$cc=[]
+  #$cc.clear
+ CSV.open("#{Rails.root}/public/files/output.csv","w", {:col_sep => "\t",:row_sep => "\r\n"}) do |csv|
+
+  csv << ["Item ID","QA_Manufacturer Name","Value"]
+
+      @pilot1= File.open(file1.tempfile,"r").collect{|p| p.chomp.split("\t")}
+
+      @pilot1.shift
+
+      @pilot2= File.open(file2.tempfile,"r").collect{|p| p.chomp.split("\t")}
+     
+      @pilot2.shift
+     
+@pilot1.map do |p|
+
+  c = []
+  @pilot2.map do |q|
+   if p[1] == q[0]
+    c << [p[0],p[1],q[1]]
+    end
+  end
+   if c.empty?
+     csv << [p[0],p[1],"#NA"].flatten
+   else
+     csv << c.flatten
+   end
+  end
+ end
+  end
+
+
+  def self.extract_attributes(file1,file2)
+    f1,f2=[],[]
+    a,b,c,d=[],[],[],[]
+    h=[]
+    f1=CSV.read(file1.tempfile)
+    f2=CSV.read(file2.tempfile)
+    h=f1.take(1)
+    h1=f2.take(1).flatten
+    h=h.flatten
+    cn=h.count
+    f1=f1.drop(1)
+    f2=f2.drop(1)
+    f1.each do |i|
+      f2.each do |j|
+        if i[0]==j[0] and i[1]==j[1]
+          c << j.take(19)
+          d << j.last(2)
+          j.each do |k|
+            unless h.index(k).nil?
+              temp=h.index(k)
+              #next if i[temp].nil?
+              if temp.nil?
+                a<<[k,nil,nil]
+              else
+                a<<[k,i[temp],i[temp+1]]
+              end
+            end
+          end
+          break if i[0]==j[0] and i[1]==j[1]
+        end
+      end
+      b<<[[c,a].flatten.insert(h1.index(h1[-2]),d.flatten[0]).insert(h1.index(h1[-1]),d.flatten[1])].flatten
+      a.clear
+      c.clear
+      d.clear
+    end
+    CSV.open("#{Rails.root}/public/files/Output_file.csv","w") do |csv|
+      csv << h1.flatten
+      b.each do |p|
+        csv << p
+      end
+    end
+end
+
+
+  end
